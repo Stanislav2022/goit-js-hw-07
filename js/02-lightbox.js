d4f3ce6441ec6e4ed57fb61b79cde0210fe3e77b
Stanislav2022 <tsv5@ukr.net>
2022-07-18 22:40:05 +0300

    hw7/2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+
+const galleryIsReady = galleryItems.map((item) => 
+`<a class="gallery__item" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryIsReady);
+
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+})
